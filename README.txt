ff980a3b53a00f9f53e51b7b532755d6df8e88b0
AJFlareau <andrew.flareau@myds.us>
2017-01-19 10:45:34 -0800

    New Day New Work

---
+++ README.txt
@@ -0,0 +1,2 @@
+Andrew Flareau
+Ian Pope
--- Tkinter Project.py
+++ Tkinter Project.py
@@ -27,26 +27,22 @@ def buttonpress():
 root = Tk() #gives us a blank canvas object to work with
 root.title("The Book of Random Contacts")
 
-button1 = Button(root, text="Enter", bg="white", command=buttonpress)
-button1.grid(row=5, column=1)
 
-entry1 = Entry(root)
-entry1.grid(row=6, column=1)
 
 
 label1 = Label(root, text="Contacts", bg="grey", anchor=W)
 label1.grid(row=1, column=0, sticky=W)
 
 image = Image.open("Logo.png")
-image = image.resize((1560,246))
+image = image.resize((780,123))
 photo = ImageTk.PhotoImage(image)
 
 scrollbar = Scrollbar(root, orient=VERTICAL)
 listbox1 = Listbox(root, yscrollcommand=scrollbar.set)
 scrollbar.config(command=listbox1.yview)
-scrollbar.grid(row=9, column=0, rowspan=10)
-listbox1.grid(row=8, column=0, columnspan=7, sticky=SW, rowspan=4)
-
+scrollbar.grid(row=2, column=5, rowspan=5, sticky=W)
+listbox1.grid(row=2, column=0, columnspan=5, rowspan=5)
+listbox1.insert(END, "Andrew Flareau", "Ian Pope")
 
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
@@ -57,10 +53,10 @@ filemenu.add_command(label="Save", command=openfileW)
 menubar.add_cascade(label="File", menu=filemenu)
 
 root.config(menu=menubar)
+ 
 
-
-logo = Label(root, image=photo)
-logo.image = photo
-logo.grid(row=0, column=0) 
+#logo = Label(root, image=photo)
+#logo.image = photo
+#logo.grid(row=0, column=0, columnspan=8) 
 
 mainloop() #causes the windows to display on the screen until program closes
